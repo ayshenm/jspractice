f38d2540681fff25dcb5fd9b49d1a6ce57d74867
ayshenm <84517249+ayshenm@users.noreply.github.com>
2021-08-18 12:29:29 +0400

    javascript temeller
    18.08.2021;12:29

---
+++ js practice/js/index.js
@@ -0,0 +1,437 @@
+/*arreyler
+let value;
+
+const numbers = [5,12,18,22,32,1,8,9];
+const numbers2 = new Array(1,2,3,4,5,6);//yarada bilerik
+
+const langs = ["phyton","java","c++","Javascript"];
+const a = ["hello",22,null,undefined,3.17];
+value = numbers.length;//uzunlugun verecek
+value = numbers[5];//indexi yazdirir
+value = numbers[7];//yada
+value = numbers[numbers.length-1];
+
+//her hansi indexi deyistirmek numbers[2] = 1000;
+value = numbers;
+
+indexof reqem hansi indexde oldugunu tapir
+value =numbers.indexOf(1000);
+
+array sonuna deyer elave etmek -push
+numbers.push(70);
+value = numbers;
+
+arreyin evveline deyer elave etme
+numbers.unshift(99);
+value = numbers;
+
+arreyin sonundan deyer silme
+numbers.pop();
+value = numbers;
+
+arreyin basindan deyer silme
+numbers.shift();
+value = numbers;
+
+belli reqemleri silmek meselen 3qeder sayir 3u silmir 0dan 3dek
+numbers.splice(0,3);
+value = numbers;
+
+arrey tam ters cevirme
+numbers.reverse();
+value = numbers;
+
+arrey siralama
+value = numbers.sort();//1ci reqemler baxib ardicilliqla siralayir
+
+balacadan boyuye siralamaq
+value = numbers.sort(function(x,y){
+    return x-y;
+});
+
+ boyuyden balacaya siralamaq
+value = numbers.sort(function(x,y){
+    return y-x;
+});
+console.log(value)
+
+
+
+//oBJECTLER
+let value;
+
+const programmer = {
+   name:"Aisha Mirza",
+   age: 25,
+   email:"mirzeyevamiray8@gmail.com",
+  lang: ["java","javascript","phyton"],
+
+  address:{
+       city: "Baku",
+      street: "M.Hadi 136",
+        
+   },
+    work: function(){
+    console.log("isleyir");
+
+    }
+}
+
+value = programmer;
+value = programmer.email;//veya genelolaraq
+value = programmer["email"];
+
+value=programmer.lang;//dilleri yazdirir
+
+value = programmer.address.city;//seheri almaq
+
+programmer.work();//metodu cagirirq
+
+obyektlerden yaranan arrey
+const programmers =[
+    {name: "Aisha Mirza",age:25},
+    {name:"Elcan Ramos",age:30},
+];
+value = programmers[0];
+value = programmers[0].name;
+
+console.log(value);
+
+
+Date object
+let value;
+const now = new Date();//hazirki vaxti almaq
+console.log(now);
+const date1 = new Date("2-09-1996 08:03:08");//veya
+const date2 = new Date("February 9 1996");
+const date3 = new Date ("02/09/1996");
+
+value = date1;
+value = date1.getMonth();//ayi yazdirir 0 dan baslayaraq
+value = date1.getFullYear();//ili yazdirir
+value = date1.getDate();//doguldugun gun yazdiririr
+value = date1.getDay();//heftenin gunun yazdiracaq
+
+value = date1.getHours();//saat; yazdiracaq
+value = date1.getMinutes();//deqiqeni yazdiracaq
+value = date1.getSeconds();//saniyeni yazdiracaq
+value = date1.getMilliseconds();//millisaniye
+
+tarix deyisdirme
+date1.setMonth(9);//ayi deyisdirdik
+date1.setFullYear(2021);//ili deyisdik
+date1.setDate(22);//dogum gunun deyisdik
+date1.setHours(0);//saati deyisdik
+date1.setMinutes(10);//deqiqe
+date1.setSeconds(15);//saniye
+value = date1;
+console.log(value);
+
+kosullar karsilastirma operatorlari
+
+//==;
+console.log(2==2);//beraberliyi yoxlayir true olacaq
+console.log("js"=="angular");//duz olmadigi ucun false olacaqconsole.log("2"==2);//stringe cevrildiyi ucun diger terefde true deyeri donur
+console.log(2==="2");//tipininde yoxladigi ucun false olacaq
+
+//!= beraber deyilse;
+console.log(2!==4); true
+console.log(2!==2); false
+
+ <kicikdir,>boyudur
+console.log(2<4);//true
+console.log(2>4);//false
+
+//boyuk ve ya beraberdir >=
+
+console.log(4>=2);//true\
+console.log(2<=2);//true
+console.log(1>=2);//false
+
+
+//mentiqsel operatorlar
+
+//! operatoru
+console.log(!(2==2));//false
+
+&& and operatoru hamsi true deyer olmalidir bir sehv olsa hamsin sehv qebul edir
+
+console.log(4==2)&&("Aisha"=="Aisha");//false yazilacaq
+console.log(2==2)&&(4==4);//true
+
+Or operatoru || 1dogru 5 sehv olsada dogrunu goturecek
+
+console.log((4==2)||("aisha"=="aisha"));
+console.log((2==2)||("ehmed"=="mehmed"));
+
+if else
+
+const error = true;
+
+if(error==true){
+    console.log("dogru");
+}
+
+const error =true; 
+if(error == true){
+    console.log("dogru");
+}
+console.log("fff");
+
+
+const error = false;
+
+if(error == true){
+    console.log("xeta");
+}
+else{
+   console.log("danger");//buranin islemesi yuxaridaki if e bagli
+}
+
+const user ="mmc";//"mty"
+if( user === "mmc"){
+    console.log("tapildi");
+}
+else{
+    console.log("yoxdur");
+}
+
+const prosess = "1";
+
+ if(prosess === "1"){
+    console.log("islem:1")
+}
+else if(prosess === "2"){
+    console.log("islem:2")
+}
+else if(prosess === "3"){
+ /  console.log("islem:3")
+}
+ else if(prosess === "4"){
+    console.log("islem:4")
+}
+else{
+    console.log("error");
+}
+
+
+//ternary operator
+
+const number = 120;
+if(number === 100){
+    console.log("beraber");
+}
+else{
+ console.log("beraber deyil");
+}//veya
+
+console.log(number === 100 ? "beraberdir" : "sayi beraber deyil");
+if(number === 100){//burda bir emr varsa motereze istifade etmeye bilerik
+    console.log("100dur")
+}
+belede yazmaq olar
+if (number === 100) console.log("sayi 100");
+else console.log("100 deyil");
+
+
+
+//switch case;if elselerin yerine istifade olunur
+
+const prosess = 2;//12;
+
+switch(prosess){
+    case 1:
+        console.log("islem1");
+        break;//duz oldugu teqdirde islemir sonlandirib cixir,break yazmasaq sehde olsa ondan asagidakilarda consolda yazdirilacaq
+    case 2:
+         console.log("islem2");
+         break;  
+    case 3:
+        console.log("islem3");
+        break;  
+    default:
+        console.log("gecersiz") ;  
+        break;  //yazsaqda olar yazmasaqda  hamsin yoxlayir olmasa default donur
+
+}
+
+
+//Funksiyalar IIEF ve anonim funksiyalar
+
+function hello(name = "bilgi yox",age="bilgi netu"){
+    //if(typeof name === "undefined") name ="bilgi yox";
+    //if(typeof age === "undefined") age = "bilgi netu";
+    console.log(`Isim: ${name} Yas: ${age}`);
+}
+hello("Aisha",25);//function call/funksiya cagirmaq
+//hello();//tanimlaya bilerik icine deyer yazdira bileriik
+hello("Elcan",30);//
+
+hello();//deyer yazmadigim ucun undefined yazacaq //bilgi yox bilgi netu kimi adlandira bilerik
+
+hello("elis")//name yazdiracaq yas bilgi netu qalacaq
+
+//return funksiya icinden deyer dondurme,funksiyani sonlandirir altina ne yazsaq calismayacaq
+
+function square(x){
+    //console.log(x*x);
+    return x * x;
+    console.log("hello");//hec vaxt calismaz returndan sonra
+}
+function cube(x){
+    //console.log(x*x*x);
+    return x*x*x;
+}
+let a = square(12);//belede yaza bilirik:let a = cube(square(12));
+a = cube(a);//NaN verecek return donmediyi ucun
+console.log(a);
+
+function hello(){
+   return "hello";
+}
+console.log(hello());//birbasa bele yaza bilerik
+hello();
+
+//function expression-funksiyani deyiskene beraber etme
+
+const hello = function(name){
+    console.log("hello" + name);
+};
+hello("Aisha");
+
+
+//Immediately Invoked Function Expression IIFE(tanimlandigi yerde isleyen funksiyalar)
+
+(function(name){
+    console.log("hello:" + name);
+})("aisha");//bos olanda undefined donecek
+
+
+//function ve metod eyni sheydi,
+const database ={
+    host: "localhost",
+    add: function(){
+        console.log("elavelendi");
+    },
+    get:function(){
+        console.log("elde edildi");
+    },
+    update: function(id){
+        console.log(`id: ${id} guncellendi`);
+    },
+    delete: function(id){
+        console.log(`id: ${id} silindi`);
+    }
+}
+console.log(database.host);
+
+database.add();
+
+database.delete(10);//10 id yerine kecdi
+
+
+//Looplar while,do while,for,for in
+
+//while dongusu
+
+//let i = 0;
+//while(i<10){// set odenildiyi teqdirde oz icinde donecek i++yazmasaq sonsuz dongu yaranacaq
+   
+//    console.log(i);
+
+ //   i++;//i=i+1seklinde yazmaq olar
+//}
+
+ //let i = 10;
+
+ //while(i>0){
+ //    console.log(i);
+
+     //i--;//i=i-1
+     //i = i - 2;//2defe azaltmaq
+  //   i-=2;
+
+
+ //}
+
+ //break and continue
+
+ //let i =0;
+ //while(i<10){
+ //    console.log(i);
+ //    if(i==5){
+ //        break;//dogrudusa dongu sonlanacaq
+ //    }
+ //    i++;
+ //}
+
+ let i =0;
+ while(i<10){//sonsuz donguye girir engellem ucun continueden evvel i++ yazmaliyiq
+     if(i ==3 || i==5){
+         i++;
+      continue;  //alti calistirmadan en basa gedir 
+     }
+     console.log(i);
+     i++;
+ }
+ 
+
+ // do while dongusu
+ let i =0;
+ do {
+     console.log(i);   
+     i++;//sert odenmesede bir defe calisir
+ }while(i<10);
+
+
+ //for dongusu
+
+ //const langs =["Java","Phyton","C++"];
+ //let index = 0;
+ //while (index< langs.length){
+ //    console.log(langs[index]);
+  //   index++;
+
+ //}
+
+ //const langs =["Java","Phyton","C++"];
+ //let index = 0;
+
+ //for (let index =0; index < langs.length; index++){//iterasyon tek tek gezdik elementler uzerin
+ //    console.log(langs[index]);
+
+ //}
+const langs =["Java","Phyton","C++"];
+
+langs.forEach(function(lang,index){
+    console.log(lang,index);//dyerleri js ozu gonderir
+
+});
+
+//Map() funksionu tek tek deyer almaq ucun
+
+const users = [
+    {name:"Aysen",age:25},
+    {name:"elis",age:15},
+    {name:"elcan",age:8}
+];
+const names = users.map(function(user){//aysen,elis,elcan
+    return user.name;
+});
+const ages = users.map(function(user){//25,15,8
+    return user.age;
+})
+console.log(ages);
+console.log(names);
+ 
+
+//for in funksiyasi
+
+const users = {
+    name:"Aysen",
+    age:25
+};
+for (let key in users){//keyle gezecek
+    console.log(key,users[key]);//name,age yazdiracaq
+} 
+*/
--- js practice/js/main.js
+++ js practice/js/main.js
@@ -1,288 +1,288 @@
-//xeta yazdirmaq;
+/*xeta yazdirmaq;
 
-//console.warn("fuck");
-//console.error("js danger");
-//console.clear();
+console.warn("fuck");
+console.error("js danger");
+console.clear();
 
-//primitive types;
+primitive types;
 
-//number type
-//var a = 20;//number type
-//var b = 3.14;//number type
-//var c = 40;
-//console.log(a,b,c);
-//console.log(typeof a);
+number type
+var a = 20;//number type
+var b = 3.14;//number type
+var c = 40;
+console.log(a,b,c);
+console.log(typeof a);
 
-//string type
-//var name = "Aisha";
-//console.log(name);
-//console.log(typeof name);//string
+string type
+var name = "Aisha";
+console.log(name);
+console.log(typeof name);//string
 
-//boolean type//true&false
-//var a =true;
-//console.log(a);
-//console.log( typeof a);
+boolean type//true&false
+var a =true;
+console.log(a);
+console.log( typeof a);
 
-//null type
-//var a = null;//bos bir deyerdir;
-//console.log(a);
-//console.log(typeof a);
+null type
+var a = null;//bos bir deyerdir;
+console.log(a);
+console.log(typeof a);
 
-//underfined type//tanimlanmamis deyer;
+underfined type//tanimlanmamis deyer;
 
-//var a;//bele yazib deyer vermedikde
-//a=5;
-//console.log(a);
+var a;//bele yazib deyer vermedikde
+a=5;
+console.log(a);
 
 
-//Referance types;
+Referance types;
 
-//Array type
-//var numbers =[1,2,3,4,5,6,7,8];
-//console.log(numbers);
-//console.log(numbers.length);
-//console.log(typeof numbers);
-//console.log(numbers[5]);
+Array type
+var numbers =[1,2,3,4,5,6,7,8];
+console.log(numbers);
+console.log(numbers.length);
+console.log(typeof numbers);
+console.log(numbers[5]);
 
 
-//person type
+person type
 
-//var person ={
-    //name:"Aisha Mirza",
-    //age: 25
+var person ={
+    name:"Aisha Mirza",
+    age: 25
 
-//}
-//console.log(person);
-//console.log(typeof person);//referans tiplerin hamsi type objectdi
+}
+console.log(person);
+console.log(typeof person);//referans tiplerin hamsi type objectdi
 
 
-//date type
+date type
 
-//var date = new Date();
-//console.log(date);
-//console.log(typeof date);
+var date = new Date();
+console.log(date);
+console.log(typeof date);
 
 
-//function object
+function object
 
-//var hello = function(){
-   // console.log("hello");
-//}
-//console.log("hello");
-//console.log(typeof hello);
+var hello = function(){
+    console.log("hello");
+}
+console.log("hello");
+console.log(typeof hello);
 
-//primitive and referance ferqi
+primitive and referance ferqi
 
-//var a = 10;
-//var b = a;
-//console.log(a,b);
-//a = 30;//a nin deyeri deyisecek b ise evvelki aldigi a deyerinde qalacaq
-//console.log(a,b);
+var a = 10;
+var b = a;
+console.log(a,b);
+a = 30;//a nin deyeri deyisecek b ise evvelki aldigi a deyerinde qalacaq
+console.log(a,b);
 
-//var a = [1,2,3];
-//var b = a;
-//a.push(4);
-//console.log(b);
+var a = [1,2,3];
+var b = a;
+a.push(4);
+console.log(b);
 
 
-//degiskenler var,let,const
+degiskenler var,let,const
 
-//var
+var
 
-//var name = "Aisha Mirza"
-//console.log(name);
-//name = "Aysen";
-//console.log(name);
+var name = "Aisha Mirza"
+console.log(name);
+name = "Aysen";
+console.log(name);
 
-//let
+let
 
-//let name = "Aysen Mirzeyeva";
-//let name = "en Mi";//tanimlamayacaq xeta verecek
+let name = "Aysen Mirzeyeva";
+let name = "en Mi";//tanimlamayacaq xeta verecek
 
-//console.log(name);
-//name = "Ayse";
-//console.log(name);
+console.log(name);
+name = "Ayse";
+console.log(name);
 
-//const
+const
 
-//const name = "Aysen Mirzeyeva";
-//console.log(name);
-//name = "dfghfjhf";
-//console.log(name);//type error verecek tekrar deyer vermek olmaz
+const name = "Aysen Mirzeyeva";
+console.log(name);
+name = "dfghfjhf";
+console.log(name);//type error verecek tekrar deyer vermek olmaz
 
-//const = a;//yaninda deyerini tanimlamaq lazimdi yoxsa xeta verecek
-//a = 10;
-//console.log(a);
+const = a;//yaninda deyerini tanimlamaq lazimdi yoxsa xeta verecek
+a = 10;
+console.log(a);
 
-//const c = [1,2,3,4,5,6,7,8,9];
+const c = [1,2,3,4,5,6,7,8,9];
 
-//console.log(c);
-//c = [1,5,8,9,7,6];
-//console.log(c);//type xetasi verecek;
-//c.push(10);//a referans etdiyi yeri deyisdiyimiz ucun xeta vermedi
-//console.log(c);
+console.log(c);
+c = [1,5,8,9,7,6];
+console.log(c);//type xetasi verecek;
+c.push(10);//a referans etdiyi yeri deyisdiyimiz ucun xeta vermedi
+console.log(c);
 
-//type deyisimi
+type deyisimi
 
-//let value;
-//veritipin stringe cevirme
+let value;
+veritipin stringe cevirme
 
-//value = 123
-//console.log(typeof value);//number
+value = 123
+console.log(typeof value);//number
 
-//number type  stringe cevirme
+number type  stringe cevirme
 
-//value = String(123);
-//console.log(typeof value);
-//value = String(true);
-//console.log(typeof value);
+value = String(123);
+console.log(typeof value);
+value = String(true);
+console.log(typeof value);
 
-//alternativ
-//value = (20).toString();
+alternativ
+value = (20).toString();
 
-//veritipin sayiya cevirme,veri tipi cevrilmirse NaN deyer donecek
-//value = Number("123");
-//value = Number(null);
-//value = Number(undefined);//cevire bilmediyimiz ucun Nan (bir sayi deyil);
-//value = Number("hello");//cevirlmeyecek NaN;
-//value = Number(function(){console.log()});//cevirlmeyecek NaN;
-//value = Number([1,2,3,5,4,85]);//cevirlmeyecek NaN;
+veritipin sayiya cevirme,veri tipi cevrilmirse NaN deyer donecek
+value = Number("123");
+value = Number(null);
+value = Number(undefined);//cevire bilmediyimiz ucun Nan (bir sayi deyil);
+value = Number("hello");//cevirlmeyecek NaN;
+value = Number(function(){console.log()});//cevirlmeyecek NaN;
+value = Number([1,2,3,5,4,85]);//cevirlmeyecek NaN;
 
-//reqemler number e cevrilir ve null (0)deyeri;
+reqemler number e cevrilir ve null (0)deyeri;
 
-//value = Number("3.14");//cevrilecek
+value = Number("3.14");//cevrilecek
 
-//const b = "aisha" + 25;
-//console.log(b);
-//const c = "25"+56;
-//console.log(c);//toplamaq istesek
-//const c = Number("25")+56;
-//console.log(c);
+const b = "aisha" + 25;
+console.log(b);
+const c = "25"+56;
+console.log(c);//toplamaq istesek
+const c = Number("25")+56;
+console.log(c);
 
-//console.log(value);
-//console.log(typeof value)
+console.log(value);
+console.log(typeof value)
 
 
-//Math operatorlari
+Math operatorlari
 
-//let value;
+let value;
 
-//const value1 = 10;
-// const value2 = 4;
+const value1 = 10;
+/const value2 = 4;
 
-//arikmatik operatorlar
-//console.log(value1 + value2);//toplama
-//console.log(value1 - value2);//cixma
-//console.log(value1 * value2);//vurma
-//console.log(value1 / value2);//bolme
-//console.log(value1 % value2);//agacli bolme yolu
+arikmatik operatorlar
+console.log(value1 + value2);//toplama
+console.log(value1 - value2);//cixma
+console.log(value1 * value2);//vurma
+console.log(value1 / value2);//bolme
+console.log(value1 % value2);//agacli bolme yolu
 
-//value = Math.PI;//pi nin deyerin yazdirir
+value = Math.PI;//pi nin deyerin yazdirir
 
-//value = Math.round(3.6);//sayilari yuxari yuvarlayir (4);3.5den asagini asagi yuvarlayir
-//value = Math.round(3.2);//3olacaq
+value = Math.round(3.6);//sayilari yuxari yuvarlayir (4);3.5den asagini asagi yuvarlayir
+value = Math.round(3.2);//3olacaq
 
-//value = Math.ceil(3.2)//direk yuxari yuvarayir 4 yazacaq;
-//value = Math.ceil(3.7);//4verecek yene
+value = Math.ceil(3.2)//direk yuxari yuvarayir 4 yazacaq;
+value = Math.ceil(3.7);//4verecek yene
 
-//value = Math.floor(3.2);//asagi yuvarlayacaq 3 yazacaq
-//value = Math.floor(3.7);//yene 3yuvarlayacaq
+value = Math.floor(3.2);//asagi yuvarlayacaq 3 yazacaq
+value = Math.floor(3.7);//yene 3yuvarlayacaq
 
-//value = Math.sqrt(16);//kok alma
-//value = Math.sqrt(31);
+value = Math.sqrt(16);//kok alma
+value = Math.sqrt(31);
 
-//value = Math.abs(-10);//modulun alir 
-//value = Math.abs(-50);
+value = Math.abs(-10);//modulun alir 
+value = Math.abs(-50);
 
-//value = Math.pow(8,3)//kub ve kvadrati tapmaq ucun
-//value = Math.pow(2,2);
+value = Math.pow(8,3)//kub ve kvadrati tapmaq ucun
+value = Math.pow(2,2);
 
-//value = Math.max(100,35,1000,85);//en yuksek sayini yazdirir
-//value = Math.min(100,35,1000,85);//en kiciyini yazdirir
+value = Math.max(100,35,1000,85);//en yuksek sayini yazdirir
+value = Math.min(100,35,1000,85);//en kiciyini yazdirir
 
 
-//value = Math.random();//0ve 1 arasinda rastgele deyer yazdiri 0 daxildir 1 daxil deyil;
-//value = Math.random() *20;//0-la 20arasinda deyerler yazdiracaq
-//value = Math.random() * 20 +1;//1le 20arasinda deyerleri yazdirir.
-//value = Math.floor( Math.random() * 20 +1);//onluq deyer kimi istedikde bele yaziriq
-//console.log(value);
+value = Math.random();//0ve 1 arasinda rastgele deyer yazdiri 0 daxildir 1 daxil deyil;
+value = Math.random() *20;//0-la 20arasinda deyerler yazdiracaq
+value = Math.random() * 20 +1;//1le 20arasinda deyerleri yazdirir.
+value = Math.floor( Math.random() * 20 +1);//onluq deyer kimi istedikde bele yaziriq
+console.log(value);
 
 
-//string metodlari
+string metodlari
 
-//let value;
-//const firstName = "Ayisha";
-//const lastName = "Mirzeyeva";
-//const langs = "Java,Phyton,C++";
+let value;
+const firstName = "Ayisha";
+const lastName = "Mirzeyeva";
+const langs = "Java,Phyton,C++";
 
-//value = firstName + " "+ lastName;//bosluq buraxmasaq bitisik yazilacaq
-//value = "Elcan Huseynov";
-//value+="Roma"//value = value + "Roma";
-//value = firstName.length;//nece karakter oldugunu yazdirir
+value = firstName + " "+ lastName;//bosluq buraxmasaq bitisik yazilacaq
+value = "Elcan Huseynov";
+value+="Roma"//value = value + "Roma";
+value = firstName.length;//nece karakter oldugunu yazdirir
 
-//string metodlari
-//concat
+string metodlari
+concat
 
-//value = firstName.concat(" ",lastName," ",langs);//uc uca string elave etmek ucun
- //tolowercase()
-//value = firstName.toLowerCase();//butun herfleri balaca yazir
-//touppercase()
-//value = firstName.toUpperCase();//butun herfleri boyuk yazir
-//index yazdirmaq
-//value = firstName[5];//index yazdirmaq(a(0-index),y(1),i(2),s(3),h(4),a(5))
-//value = firstName.length -1;//son indexi yazdiracaq;ve yaxud
-//value = firstName[firstName.length - 1];//seklindede yazmaq olar stringin uzunlugun bilmediyimz durumlarda
+value = firstName.concat(" ",lastName," ",langs);//uc uca string elave etmek ucun
+ tolowercase()
+value = firstName.toLowerCase();//butun herfleri balaca yazir
+touppercase()
+value = firstName.toUpperCase();//butun herfleri boyuk yazir
+index yazdirmaq
+value = firstName[5];//index yazdirmaq(a(0-index),y(1),i(2),s(3),h(4),a(5))
+value = firstName.length -1;//son indexi yazdiracaq;ve yaxud
+value = firstName[firstName.length - 1];//seklindede yazmaq olar stringin uzunlugun bilmediyimz durumlarda
 
-//indexof  karakterin harda oldugunu gosterir herfi yazmaqla hansi indexde oldugunu yazdirir
+indexof  karakterin harda oldugunu gosterir herfi yazmaqla hansi indexde oldugunu yazdirir
 
-//value = firstName.indexOf("h");//4cu yerde oldugunu yazdirdi
-//value = firstName.indexOf("c");//olmayan herf axtardiqda -1 dyerin q2aytarir
+value = firstName.indexOf("h");//4cu yerde oldugunu yazdirdi
+value = firstName.indexOf("c");//olmayan herf axtardiqda -1 dyerin q2aytarir
 
-//charat()
-//value = firstName.charAt(0);//index qeyd etmekle hemin indexdeki herfi yazdiri
-//value = firstName.charAt(firstName.length - 1);//sonuncu herfi alir
+charat()
+value = firstName.charAt(0);//index qeyd etmekle hemin indexdeki herfi yazdiri
+value = firstName.charAt(firstName.length - 1);//sonuncu herfi alir
 
 
-//split//parcalayir
+split//parcalayir
 
-//value = langs.split(",");//vergule gore parcalayib ayrica arreyde yazdiracaq
+value = langs.split(",");//vergule gore parcalayib ayrica arreyde yazdiracaq
 
 
-//replace()//deyerin yerine basqa deyer yazdirma
+replace()//deyerin yerine basqa deyer yazdirma
 
-//value = langs.replace("Phyton","css");
+value = langs.replace("Phyton","css");
 
-//includes//sorgulamaq ucun yeni deyer varmi yoxmu
-//value = langs.includes("Java");//tapsa true donur
-//value = langs.includes("asp-net");//tapmasa false donur
-//console.log(value);
+includes//sorgulamaq ucun yeni deyer varmi yoxmu
+value = langs.includes("Java");//tapsa true donur
+value = langs.includes("asp-net");//tapmasa false donur
+console.log(value);
 
 
-//template literal
-//const name = "Aisha Mirza";
-//const department = "Texnologiya";
-//const salary = 8000;
+template literal
+const name = "Aisha Mirza";
+const department = "Texnologiya";
+const salary = 8000;
 
-//const a = "Isim:" + name +"\n"+"bolum:"+department+"\n"+"maas:"+salary;
-//daha asan yazilis
-//const a = `isim:${name}\nbolum:${department}\nmaas:${8000}` 
-//body yazi yazdirma
-  //const html = "<ul>" +
-  //             "<li>"+ name + "</li>" +
-  //             "<li>"+ department + "</li>"+
-  //              "<li>" + salary + "</li>"+
-  //              "</ul>";
+const a = "Isim:" + name +"\n"+"bolum:"+department+"\n"+"maas:"+salary;
+daha asan yazilis
+const a = `isim:${name}\nbolum:${department}\nmaas:${8000}` 
+body yazi yazdirma
+  const html = "<ul>" +
+               "<li>"+ name + "</li>" +
+               "<li>"+ department + "</li>"+
+                "<li>" + salary + "</li>"+
+                "</ul>";
 
-//template literal ile daha asan yazi
+template literal ile daha asan yazi
 
-//const html = `
- //       <ul>
- //      <li>${name}</li>
-  //      <li>${department}</li>
-  //      <li>${salary}</li>
-  //      </ul>
+const html = `
+        <ul>
+       <li>${name}</li>
+        <li>${department}</li>
+        <li>${salary}</li>
+        </ul>
 
 
 `;
-//document.body.innerHTML = html;
-
+document.body.innerHTML = html;
+*/
